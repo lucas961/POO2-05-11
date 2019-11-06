7c16ff4d1639d13400a6424f84f6b340db2a7f81
lucas Izidorio <lucasdia14@gmail.com>
2019-11-05 21:11:49 -0300

    Com comentário

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-
+#Variaveis#
 a = 1
 b = -5
-
+#Soma das variaveis#
 soma = a + b
+#Comparação maior que a#
 if(soma >a):
+    #verificação de maior que a#
     print("soma maior que a")
+    #verificação de igual a a#
 elif(soma == a)
     print("soma igual a a")
+    #verificação de menor que a#
 else: 
-    print("soma menor que a")        
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+    print("soma menor que a")
